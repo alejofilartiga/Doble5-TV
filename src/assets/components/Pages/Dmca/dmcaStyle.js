23e6a248d9aec8a684c2d938361e4be95813aa74
Alejo <alejofilartiga@gmail.com>
2025-02-15 00:34:25 -0300

    DMCA Policy page added

---
+++ src/assets/components/Pages/Dmca/dmcaStyle.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const DmcaStyled = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+`
+
+export const InfoContainer = styled.div`
+    padding-top: 100px;
+    padding-bottom: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: white;
+    text-align: center;
+    width: 90%;
+`
+
+
+export const DmcaTitle = styled.h2`
+    font-size: 50px;
+    text-align: center;
+`
+
+export const InfoText = styled.p`
+    font-size: 20px;
+    text-align: center;
+    width: 80%;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    line-height: 1.5;
+    color: white;
+    text-align: justify;
+    text-justify: inter-word;
+    font-family: 'Roboto', sans-serif;
+
+`
